29ffe278f2c9d4953b9595c2d8b62d29bc40dced
caiquangtung <caitung8@gmail.com>
2024-11-01 14:32:39 +0700

    GatewaySvc

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+478c12ce55ae575b11e443035cc94f5c4cd01c2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e13035d9cc106862a3e80616feb79fad6250eef")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
